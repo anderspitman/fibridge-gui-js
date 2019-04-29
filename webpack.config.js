281f7b8dbfa1e26533a00909d3716209516e235e
Anders Pitman <tapitman11@gmail.com>
2019-04-29 09:19:48 -0600

    Switch to webpack
    Wasn't able to quickly find an html plugin with js inlining for
    rollup.

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
+module.exports = {
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      // embed all javascript and css inline in HTML file
+      inlineSource: '.(js|css)$'
+    }),
+    new HtmlWebpackInlineSourcePlugin()
+  ],
+};
